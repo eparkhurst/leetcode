7c96a0de9c2c9e6c6ee58e894ba1b6de841d677d
Elijah Parkhurst <eli@bouncecon.com>
2023-11-03 16:47:46 -0600

    got my sudoku solver to work

--- generateParens.js
+++ generateParens.js
@@ -5,7 +5,7 @@
 var generateParenthesis = function (n) {
     let ans = ['('];
     for (let i = 0; i < n * 2 - 1; i++) {
-        nextAns = [];
+        const nextAns = [];
         for (let j = 0; j < ans.length; j++) {
             if (ans[j].match(/\(/g).length < n) nextAns.push(`${ans[j]}(`);
             if (ans[j].match(/\(/g).length > (ans[j].match(/\)/g) || []).length) nextAns.push(`${ans[j]})`);
